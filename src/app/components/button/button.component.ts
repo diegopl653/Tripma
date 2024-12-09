329c2220552d576cfc12387296d6388cd334b29c
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2024-12-08 21:30:26 -0400

    Fix: create buttonClasses signal computed to return the classes for the button
    Use this to add a class to the button component and minimize the code in the template

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { NgModule, OnInit } from '@angular/core';
+import { computed, NgModule, OnInit, Signal } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { Component, input, Input, signal } from '@angular/core';
@@ -18,4 +18,22 @@ export class ButtonComponent {
   @Input() destructive: boolean = false;
   @Input() onClick: () => void = () => {};
   @Input() texto: string = '';
+
+  buttonClasses = computed(() => ({
+    button: true,
+    'size-small': this.size === 'small',
+    'size-large': this.size === 'large',
+    'type-primary': this.type === 'primary' && !this.destructive,
+    'type-primary-destructive': this.type === 'primary' && this.destructive,
+    'type-secondary': this.type === 'secondary' && !this.destructive,
+    'type-secondary-destructive': this.type === 'secondary' && this.destructive,
+    'type-tertiary': this.type === 'tertiary' && !this.destructive,
+    'type-tertiary-destructive': this.type === 'tertiary' && this.destructive,
+    'icon-only': this.iconOnly,
+  }
+));
+
+  ngOnInit() {
+    console.log('buttonClasses', this.buttonClasses());
+  }
 }
